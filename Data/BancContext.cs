d29e7d75530ee3724ec2458d96d1c57411d5ed3b
KayoFontesLunga <kayolunga84@gmail.com>
2025-02-22 00:53:11 -0300

    :construction: In progress. Connection to the Database

---
+++ Data/BancContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactControl.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactControl.Data
+{
+    public class BancContext : DbContext
+    {
+        public BancContext(DbContextOptions<BancContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ContactModel> Contacts { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ContactControl.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BancContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase")));
 
 var app = builder.Build();
 
